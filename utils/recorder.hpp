3d7b9b88c34042b40572e0778ab12fcc3811bf3a
qingfengava <19972408019@163.com>
2026-05-14 10:58:29 +0800

    fix

--- utils/recorder.hpp
+++ utils/recorder.hpp
@@ -4,7 +4,9 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <iostream>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <sstream>
 #include <string>
